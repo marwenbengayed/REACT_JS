1ed0524e5eda62bf58660843d6c10efeae43d5ba
marwenbengayed <marobengayed@gmail.com>
2023-11-01 17:14:39 +0100

    update

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,7 +1,5 @@
 /* bootstrap CSS file */
 import 'bootstrap/dist/css/bootstrap.min.css';
-/* Importing fragment as named module */
-import { Fragment } from "react";
 /* Importing Navbar component*/
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -11,15 +9,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Card from 'react-bootstrap/Card';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import React from 'react';
 
 function MyComponent() {
-    return (
-        <Fragment>
-            {
+    return(
+        
+        <React.Fragment>
+            
                 <div className="App">
-                    {
-                        // Navbar Component: Link and Dropdown list
-                    }
+                    
+                        {/* // Navbar Component: Link and Dropdown list */}
+                    
                     <Navbar expand="lg" className="bg-body-tertiary">
                         <Container>
                             <Navbar.Brand href="#home">React-Bootstrap Navbar</Navbar.Brand>
@@ -43,16 +43,15 @@ function MyComponent() {
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
-                    {
-                        // Header Component: Simple text and subtext
-                    }
+                                            {/* // Header Component: Simple text and subtext */}
+                
                     <header>
                         <h1>Page header </h1><small>Subtext for header</small>
                     </header>
                     <br></br>
-                    {
-                        // Card1 Component: basic card 
-                    }
+                    
+                        {/* // Card1 Component: basic card  */}
+                    
                     <Card border="primary" style={{ width: '18rem' }}>
                         <Card.Body>
                             <Card.Title>Card 1</Card.Title>
@@ -63,9 +62,9 @@ function MyComponent() {
                         </Card.Body>
                     </Card>
                     <br></br>
-                    {
-                        // Card2 Component: Title, text, and links 
-                    }
+                    
+                        {/* // Card2 Component: Title, text, and links  */}
+                    
                     <Card border="success" style={{ width: '18rem' }}>
                         <Card.Body>
                             <Card.Title>Card 2</Card.Title>
@@ -79,9 +78,9 @@ function MyComponent() {
                         </Card.Body>
                     </Card>
                     <br></br>
-                    {
-                        // Card3 Component: Footer, Title, text, and Footer 
-                    }
+                    
+                        {/* // Card3 Component: Footer, Title, text, and Footer  */}
+                    
                     <Card border="danger" style={{ width: '18rem' }} className="text-center">
                         <Card.Header>Card 3</Card.Header>
                         <Card.Body>
@@ -105,9 +104,9 @@ function MyComponent() {
                         <Card.Footer className="text-muted">Card Footer</Card.Footer>
                     </Card>
                 </div>
-            }
-        </Fragment>
-    );
+            
+            </React.Fragment>
+)
 }
 
 export default MyComponent;
